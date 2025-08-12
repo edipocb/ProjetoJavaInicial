1b69b85301d12f3ac81fb724e2775f0a1015935a
FIC <fic@info.local>
2025-08-11 21:49:16 -0300

    Adcionando projeto exercicios funcoes

---
+++ ExerciciosFuncoes/src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    // 1. Exibir uma mensagem de boas-vindas
+    public static void mostrarMensagem() {
+        System.out.println("Bem-vindo(a) ao programa");
+    }
+
+    // 2. Mostrar o dobro de um número
+    public static void mostrarDobro(int num1){
+        int resultado = num1 * 2;
+        System.out.println(resultado);
+    }
+
+    //3. Retornar a divisão de dois números inteiros
+    public static int dividir(int num2, int num3) {
+        int divisao = num2 / num3;
+        return divisao;
+
+
+    }
+    //4. Verificar se um número é par
+//    public static int ehPar( int num4){
+//        int parImpar = num4 % 10;
+//        return parImpar;
+//    }
+
+    public static void main(String[] args) {
+    mostrarMensagem();
+    mostrarDobro(15);
+
+//    int dividir = dividir(20,2);
+//        System.out.println(dividir);
+
+
+
+
+    }
+}

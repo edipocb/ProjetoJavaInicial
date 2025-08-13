37eaff1fc54f48c243a87da96b7890ce506a3dae
FIC <fic@info.local>
2025-08-12 21:39:59 -0300

    Exercicios funcoes

--- ExerciciosFuncoes/src/main.java
+++ ExerciciosFuncoes/src/main.java
@@ -1,9 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// fluxo criar funcao
+// 1. Criar esqueleto d funcao
+// 2. Definir se tem retorno ou nao
+// 3. Definir se tem parametro ou nao
+
 public class main {
 
     // 1. Exibir uma mensagem de boas-vindas
     public static void mostrarMensagem() {
+
         System.out.println("Bem-vindo(a) ao programa");
     }
 
@@ -20,9 +27,39 @@ public class main {
 
 
     }
-    //4. Verificar se um número é par
+
+
+    //Extra 1 - Converter Celsius para Fahrenheit
+    public static double converterCelsiusParaFahrenheit(double celsius) {
+        return celsius * 1.8 + 32;
+    }
+
+    //Extra 2 - Calcular Área de um retângulo
+    public static double calcularAreaRetangulo(double base, double altura) {
+        double area;
+        return area = base * altura;
+    }
+
+    //Intermediários- 4. Verificar se um número é par
     public static boolean ehPar(int num4){
+
         return num4 % 2 == 0;
+    }
+
+    //Intermediários -5. Calcular média de três notas
+
+    public static double calcularMedia(double nota1, double nota2, double nota3) {
+
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    // Intermediários 6. Mostrar uma linha decorativa
+
+    public static void linha(){
+        for (int i = 0; i < 30 ; i++) {
+            System.out.print("-");
+
+        }
     }
 
     public static void main(String[] args) {
@@ -39,5 +76,18 @@ public class main {
         } else {
             System.out.println(valor + " É impar ");
         }
+
+        double celsius = 20;
+        double resultado = converterCelsiusParaFahrenheit(celsius);
+        System.out.println(resultado);
+        //converterCelsiusParaFahrenheit(30)
+
+        double calculo = calcularAreaRetangulo(10, 15);
+        System.out.println("Area do retanculo e: " + calculo);
+
+        double media = calcularMedia(7.5, 6.5, 7.0);
+        System.out.println("A media e: " + media);
+
+        linha();
     }
 }
